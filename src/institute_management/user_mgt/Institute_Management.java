c8b1ce194e5afb38203709a3075e84886b33a6d8
TharinduMalinda <rtharindumalinda@gmail.com>
2017-02-22 09:14:39 +0530

    init

--- src/institute_management/user_mgt/Institute_Management.java
+++ src/institute_management/user_mgt/Institute_Management.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package institute_management;
+package institute_management.user_mgt;
 
 /**
  *
