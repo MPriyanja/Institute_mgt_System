b14dc46a70a0c16a6c916263516d85eba6d0c62f
mihiran <mihiran@Mihiran>
2017-02-22 00:32:24 -0800

    MIHIRAN

--- src/institute_management/Institute_Management.java
+++ src/institute_management/Institute_Management.java
@@ -16,7 +16,7 @@ public class Institute_Management {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello wORLD");
+        System.out.println("Hello meeya");
     }
     
 }
