b6aa27e75767fecedcdc6ed1e8ba70213a1b6aec
MPriyanja <mpriyanja@gmail.com>
2017-03-16 09:01:59 +0530

    add student

--- src/com/institute_management/subject_mgt/DB/SubjectDbConnection.java
+++ src/com/institute_management/subject_mgt/DB/SubjectDbConnection.java
@@ -5,6 +5,7 @@
  */
 package com.institute_management.subject_mgt.DB;
 
+import com.institute_management.student.BEAN.Student;
 import com.institute_management.subject_mgt.BEAN.SubjectBean;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -116,5 +117,89 @@ public class SubjectDbConnection {
             }
         }
         return subjectList;
+    }
+
+    public int insertStudent(Student st) throws Exception {
+        int count = 0;
+
+        try {
+            query = "INSERT INTO student(S_NAME, S_DOB, S_ADDRESS, S_EMAIL, S_GENDER, S_YOR, S_TELEPHONE, S_SCHOOL) VALUES (?,?,?,?,?,?,?,?)";
+            pst = connection.prepareStatement(query);
+            pst.setString(1, st.getName());
+            pst.setString(2, st.getDob());
+            pst.setString(3, st.getAddress());
+            pst.setString(4, st.getEmail());
+            pst.setString(5, st.getGender());
+            pst.setString(6, st.getYearOfReg());
+            pst.setString(7, st.getTelephn());
+            pst.setString(8, st.getSchool());
+            //pst.setString(6, st.getYearOfReg());
+            count = pst.executeUpdate();
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            try {
+                if (pst != null) {
+                    try {
+                        pst.close();
+                    } catch (SQLException e) {
+                        throw e;
+                    }
+                }
+            } catch (Exception ee) {
+                throw ee;
+            }
+        }
+        return count;
+    }
+
+    public ArrayList<Object[]> selectAllStudent() throws Exception{
+        ArrayList<Object[]> studentList = new ArrayList<Object[]>();
+        Object[] tableData = null;
+        try {
+            query = "SELECT  S_NAME,S_DOB,S_ADDRESS,S_EMAIL,S_GENDER,S_YOR,S_TELEPHONE,S_SCHOOL FROM student";
+
+            pst = connection.prepareStatement(query);
+
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                tableData = new Object[8];
+                //tableData[0] = false;
+                tableData[0] = rs.getString("S_NAME");
+                tableData[1] = rs.getString("S_DOB");
+                tableData[2] = rs.getString("S_ADDRESS");
+                tableData[3] = rs.getString("S_EMAIL");
+                tableData[4] = rs.getString("S_GENDER");
+                tableData[5] = rs.getString("S_YOR");
+                tableData[6] = rs.getString("S_TELEPHONE");
+                tableData[7] = rs.getString("S_SCHOOL");
+
+                studentList.add(tableData);
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            try {
+                if (pst != null) {
+                    try {
+                        pst.close();
+                    } catch (SQLException e) {
+                        throw e;
+                    }
+                }
+                if (rs != null) {
+                    try {
+                        rs.close();
+                    } catch (SQLException e) {
+                        throw e;
+                    }
+                }
+            } catch (Exception ee) {
+                throw ee;
+            }
+        }
+        return studentList;
     }
 }
